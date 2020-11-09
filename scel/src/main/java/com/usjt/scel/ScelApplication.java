fdd167734dbceb67fe2e68134479c4f3670d1df9
probook <engsoftware1@gmail.com>
2020-11-09 15:45:15 -0200

    versão inicial

---
+++ scel/src/main/java/com/usjt/scel/ScelApplication.java
@@ -0,0 +1,15 @@
+package com.usjt.scel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * teste para o revert
+ */
+@SpringBootApplication
+public class ScelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScelApplication.class, args);
+	}
+
+}
